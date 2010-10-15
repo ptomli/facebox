78f9f37b339235dd018e62a1a493412e0b0376d5
Paul Tomlin <pault@bulksms.com>
2010-10-15 14:18:51 +0200

    use CSS border and border-radius instead of border images

--- facebox.js
+++ facebox.js
@@ -88,29 +88,15 @@
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
       <div class="popup"> \
-        <table> \
-          <tbody id="facebox-tbody"> \
-            <tr> \
-              <td class="tl"/><td class="b"/><td class="tr"/> \
-            </tr> \
-            <tr> \
-              <td class="b"/> \
-              <td class="body"> \
-                <div class="content"> \
-                </div> \
-                <div class="footer"> \
-                  <a href="#" class="close"> \
-                    <img src="closelabel.gif" title="close" class="close_image" /> \
-                  </a> \
-                </div> \
-              </td> \
-              <td class="b"/> \
-            </tr> \
-            <tr> \
-              <td class="bl"/><td class="b"/><td class="br"/> \
-            </tr> \
-          </tbody> \
-        </table> \
+        <div class="body"> \
+          <div class="content"> \
+          </div> \
+          <div class="footer"> \
+            <a href="#" class="close"> \
+              <img src="closelabel.gif" title="close" class="close_image" /> \
+            </a> \
+          </div> \
+        </div> \
       </div> \
     </div>'
     };
@@ -147,7 +133,7 @@
       $('#facebox .content').append(data);
       $('#facebox .loading').remove();
       $('#facebox .body').children().fadeIn('normal');
-      $('#facebox').css('left', $(window).width() / 2 - ($('#facebox table').width() / 2));
+      $('#facebox').css('left', $(window).width() / 2 - ($('#facebox .body').width() / 2));
       $(document).trigger('reveal.facebox').trigger('afterReveal.facebox');
     },
 
